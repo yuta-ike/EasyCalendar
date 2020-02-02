c256b7794e51f038b86b015bf90db147a072b81f
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-02-02 16:25:37 +0900

    add LINE BOT 雛形

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 build:
 	go get github.com/aws/aws-lambda-go/events
 	go get github.com/aws/aws-lambda-go/lambda
+	go get github.com/line/line-bot-sdk-go/linebot
 	go build -i -o ./dist/functions/calendar ./src/functions/calendar
 	go build -i -o ./dist/functions/sample ./src/functions/sample
 	go build -o ./dist/public/src ./src/public/...
--- src/functions/calendar/calendar.go
+++ src/functions/calendar/calendar.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	accessToken := os.Getenv("LINE_CHANNEL_ACCESS_TOKEN")
+	channelSecret := os.Getenv("LINE_CHANNEL_SECRET")
+	bot, err := linebot.New(channelSecret, accessToken)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eventList, err := UnmarshalLineRequest([]byte(request.Body))
+
+	for _, event := range eventList.Events {
+		replyToken := event.ReplyToken
+		message := event.Message
+		switch {
+		case event.Message.Type == "text":
+			if _, err = bot.ReplyMessage(replyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+				log.Print(err)
+			}
+		}
+	}
+
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",
