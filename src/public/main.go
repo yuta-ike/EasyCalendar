6781200fc3007068d5f4a755b393ab8ac8eae850
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2020-02-02 22:46:21 +0900

    add LIFF application n 雛形

--- src/public/main.go
+++ src/public/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Sample!!src")
+	http.HandleFunc("/*", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("clock.tpl"))
+
+	m := map[string]string{
+		"Date": time.Now().Format("2006-01-02"),
+		"Time": time.Now().Format("15:04:05"),
+	}
+
+	tpl.Execute(w, m)
+
+	// w.WriteHeader(http.StatusOK)
+	// w.Write([]byte("<html><h>HELLO</h><p>world</p></html>"))
 }
